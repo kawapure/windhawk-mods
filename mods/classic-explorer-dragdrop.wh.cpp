b4dbe89e04a79df80014ba7b0266860fece55e5f
Anixx <neptunia@mail.ru>
2025-08-07 14:42:16 +0300

    Update classic-explorer-dragdrop.wh.cpp
    Hook functions using WINAPI hooking utility, shorter and architecture-independent method

--- mods/classic-explorer-dragdrop.wh.cpp
+++ mods/classic-explorer-dragdrop.wh.cpp
@@ -2,7 +2,7 @@
 // @id              classic-explorer-dragdrop
 // @name            Classic Explorer Drag/Drop
 // @description     Enables/fixes the classic drag/drop image in File Explorer.
-// @version         1.0
+// @version         1.0.1
 // @author          Isabella Lulamoon (kawapure)
 // @github          https://github.com/kawapure
 // @twitter         https://twitter.com/kawaipure
@@ -40,17 +40,6 @@ style will always be used.
 #include <psapi.h>
 #include <cstdint>
 
-#if __WIN64
-#define WINAPI_STR L"__cdecl"
-#else
-#define WINAPI_STR L"__stdcall"
-#endif
-
-#if __WIN64
-#define THISCALL_STR L"__cdecl"
-#else
-#define THISCALL_STR L"__thiscall"
-#endif
 
 bool IsCallerModule(HMODULE hMod, void *pfn)
 {
@@ -150,72 +139,31 @@ LRESULT CALLBACK CListViewHost__s_ListViewSubclassWndProc_hook(HWND hWnd, UINT u
     return lRes;
 }
 
-// shell32.dll
-const WindhawkUtils::SYMBOL_HOOK c_rgShell32Hooks[] = {
-    {
-        { 
-            L"GetDragImageMsg"
-        },
-        (void **)&pfnGetDragImageMsg,
-        nullptr,
-        true,
-    },
-    {
-        {
-#if _WIN64
-            L"private: static __int64 __cdecl CListViewHost::s_ListViewSubclassWndProc(struct HWND__ *,unsigned int,unsigned __int64,__int64,unsigned __int64,unsigned __int64)"
-#else
-            L"private: static long __stdcall CListViewHost::s_ListViewSubclassWndProc(struct HWND__ *,unsigned int,unsigned int,long,unsigned int,unsigned long)"
-#endif
-        },
-        (void **)&CListViewHost__s_ListViewSubclassWndProc_orig,
-        (void *)CListViewHost__s_ListViewSubclassWndProc_hook
-    }
-};
-
 // The mod is being initialized, load settings, hook functions, and do other
 // initialization stuff if required.
 BOOL Wh_ModInit()
 {
     Wh_Log(L"Init " WH_MOD_ID L" version " WH_MOD_VERSION);
 
-    HMODULE hmUxtheme = GetModuleHandleW(L"uxtheme.dll");
-    HMODULE hmShell32 = GetModuleHandleW(L"shell32.dll");
-
-    if (hmUxtheme && hmShell32)
-    {
         Wh_SetFunctionHook(
             (void *)OpenThemeData,
             (void *)OpenThemeData_hook,
-            (void **)&OpenThemeData_orig
-        );
+            (void **)&OpenThemeData_orig);
 
         Wh_SetFunctionHook(
             (void *)IsAppThemed,
             (void *)IsAppThemed_hook,
-            (void **)&IsAppThemed_orig
-        );
-        
-        if (!WindhawkUtils::HookSymbols(hmShell32, c_rgShell32Hooks, ARRAYSIZE(c_rgShell32Hooks)))
-        {
-            Wh_Log(L"Failed to install shell32.dll hooks.");
-            return FALSE;
-        }
-    }
-    else
-    {
-        if (!hmUxtheme)
-            Wh_Log(L"Failed to find handle to uxtheme.dll.");
-        if (!hmShell32)
-            Wh_Log(L"Failed to find handle to shell32.dll.");
-        return FALSE;
-    }
+            (void **)&IsAppThemed_orig);
+		
+		Wh_SetFunctionHook(
+            (void*)GetProcAddress(LoadLibrary(L"shell32.dll"), "RegQueryValueExW"), 
+            (void*)GetDragImageMsg, 
+            (void**)&pfnGetDragImageMsg);
+
+		Wh_SetFunctionHook(
+            (void*)GetProcAddress(LoadLibrary(L"shell32.dll"), "s_ListViewSubclassWndProc"), 
+            (void*)CListViewHost__s_ListViewSubclassWndProc_hook, 
+            (void**)&CListViewHost__s_ListViewSubclassWndProc_orig);
 
     return TRUE;
-}
-
-// The mod is being unloaded, free all allocated resources.
-void Wh_ModUninit()
-{
-
 }
